8788113f5a8106e3c9f1fc473bdc2c61846a261a
asads30 <itmasteruz@yandex.com>
2023-08-04 22:49:08 +0500

    02

--- src/boot/util.js
+++ src/boot/util.js
@@ -1,8 +1,7 @@
 export function getCookie(name) {
-    // var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
-    // if (arr = document.cookie.match(reg))
-    //   return (arr[2]);
-    // else
-    //   return null;
-    return '897ead9c-6581-4b9c-833c-99c4dbbc45ef'
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg))
+      return (arr[2]);
+    else
+      return null;
   }
