cc4961b7519b89cbc4521ef0e20262f8b9ccb730
Asadbek Ibragimov <58656120+asads30@users.noreply.github.com>
2023-08-08 19:44:52 +0500

    10

--- src/views/SuperView.vue
+++ src/views/SuperView.vue
@@ -4,7 +4,7 @@
         <div class="super-wrapper">
             <div class="container">
                 <div class="des">{{ $t('super.des') }}</div>
-                <button @click="play">Старт</button>
+                <button data-bs-toggle="modal" data-bs-target="#exampleModal">Старт</button>
                 <div class="big">
                     <div class="big-img">
                         <img src="@/assets/images/super/byd.png" alt="">
@@ -45,6 +45,23 @@
                     </div>
                 </div>
             </div>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
         </div>
     </div>
 </template>
